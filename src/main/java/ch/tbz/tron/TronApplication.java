e47cc24a3fcb50c981549551df3abdbb6d18df59
noahburren <noah.burren@bluewin.ch>
2026-02-20 11:32:18 +0100

    init tron project

--- src/main/java/ch/tbz/tron/TronApplication.java
+++ src/main/java/ch/tbz/tron/TronApplication.java
@@ -1,4 +1,4 @@
-package org.example.tron;
+package ch.tbz.tron;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/ch/tbz/tron/TronApplicationTests.java
+++ src/test/java/ch/tbz/tron/TronApplicationTests.java
@@ -1,4 +1,4 @@
-package org.example.tron;
+package ch.tbz.tron;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
