29ee3d7a360732f2f7c6d4f0ae678e74a45f9fe6
noahburren <noah.burren@bluewin.ch>
2026-02-20 11:14:45 +0100

    init tron project

---
+++ src/main/java/org/example/tron/TronApplication.java
@@ -0,0 +1,13 @@
+package org.example.tron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TronApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TronApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/tron/TronApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TronApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
